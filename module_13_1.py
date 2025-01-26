45dbf38018c321680208dabd10b37ae777b4b093
MarinaIgnat <9702m@mail.ru>
2025-01-26 13:36:06 +0700

    Add files via upload

---
+++ module_13_1.py
@@ -0,0 +1,23 @@
+import asyncio
+
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+    await asyncio.sleep(power)
+    for i in range(6):
+        if i == 0:
+            continue
+        print(f'Силач {name} поднял {i} шар')
+    print(f'Силач {name} закончил соревнования.')
+
+
+async def start_tournament():
+    participant_1 = asyncio.create_task(start_strongman('Pasha', 3))
+    participant_2 = asyncio.create_task(start_strongman('Denis', 4))
+    participant_3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await participant_1
+    await participant_2
+    await participant_3
+
+
+asyncio.run(start_tournament())
